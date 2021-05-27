f977ebec506cf14a15e7461072c69e7ccd402681
Richard Ramirez <hackysmack69@gmail.com>
2021-05-27 03:04:53 +0000

    boolean operators

---
+++ boolean-operators.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+# logical AND, OR, NOT
+
+print("lets try not true and not false")
+print(not True)
+print(not False)
+
+print("lets try using OR")
+print(True or True)
+print(True or False)
+print(False or False)
+
+print("lets try using AND")
+print(True and True)
+print(True and False)
+print(False and False)
