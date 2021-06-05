51decef39c1dcfb49f05d017505f63c5b6e8341b
Richard Ramirez <hackysmack69@gmail.com>
2021-06-05 22:46:51 +0000

    finished input lesson

---
+++ inputfunct.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+age = input("How old are you today? ")
+print("your responded with: " + name + '~' + color + '~' + age)
