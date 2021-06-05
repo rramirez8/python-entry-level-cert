6f5836893b4e93701aba12b06df6d7b1e37f1520
Richard Ramirez <hackysmack69@gmail.com>
2021-06-05 23:08:23 +0000

    finished print exercise

--- inputfunct.py
+++ inputfunct.py
@@ -3,4 +3,14 @@
 name = input("What is your name? ")
 color = input("What is your favorite color? ")
 age = input("How old are you today? ")
-print("your responded with: " + name + '~' + color + '~' + age)
+
+#print the old fashioned way
+print("The old way")
+print(name, end=" ")
+print("is " + str(age) + " years old", end=" ")
+print("and loves the color " + color + ".")
+
+# print using the separator 
+print("The better way")
+print(name, "is", age, "years old and loves the color", color, sep=" " )
+# the default separator is a single space " "
