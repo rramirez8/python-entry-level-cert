abcc06f85bb061b6acb2179b9526e2c835aa302d
Richard Ramirez <hackysmack69@gmail.com>
2021-06-05 23:41:50 +0000

    completed immutable lesson

---
+++ immutable.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+# lesson in immutability
+mystr = "testing"
+print(mystr)
+print(mystr.capitalize())
+print(id(mystr))
+print(id("testing"))
+
+otherstr = "testing"
+print(id(mystr) == id(otherstr))
