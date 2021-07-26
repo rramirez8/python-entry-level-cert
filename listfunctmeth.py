cf51732df428e5235555ed79e9117b30659ae217
Richard Ramirez <hackysmac69@gmail.com>
2021-07-26 18:55:08 +0000

    funtions and methods

---
+++ listfunctmeth.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+# create at list
+mylist = [1,2,3]
+
+# append to the list
+mylist.append(4)
+print(mylist)
+
+# insert in the middle of the list
+mylist.insert(0, 'a')
+print(mylist)
+
+# get value of specific index
+mylist = [1,2,3]
+print(mylist)
+print(mylist.index(1))
