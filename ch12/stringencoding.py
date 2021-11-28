3531767747705e92a22fa3b3436a1ce5481e6c9d
Richard Ramirez <hackysmack69@gmail.com>
2021-11-27 22:58:58 -0600

    adding new files

---
+++ ch12/stringencoding.py
@@ -0,0 +1,8 @@
+print("We can print unicode characters as strings")
+print("utf ordinal u2122", ord('\u2122'))
+print('\u2122')
+
+print("we can also print a unicode char using the chr(int) function")
+print("example", chr(8482))
+print("use ord to go from string to utf8 number")
+print("use chr to go from utf8 number to string")
---
+++ ch12/stringfunctions.py
@@ -0,0 +1,13 @@
+print("lets look at some handy string funcitons")
+print("lower functions")
+print("THIS".lower())
+
+print("Lets look at lower, upper, capitalize, title")
+print("original string")
+mystr = "tEsTiNG"
+print(mystr)
+print("lower: ", mystr.lower())
+print("upper: ", mystr.upper())
+print("capitalize: ", mystr.capitalize())
+print("title: ", "hello hello hello".title())
+
