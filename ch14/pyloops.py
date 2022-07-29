7d43000ccd403f27711b89b71a14dc20cb7b454e
Richard Ramirez <hackysmack69@gmail.com>
2022-07-29 16:06:30 -0500

    added ch14 dir and looping exercise

---
+++ ch14/pyloops.py
@@ -0,0 +1,17 @@
+# while loop
+count = 1
+while count <= 10:
+    print("The count is: " + str(count))
+    count += 1
+
+# for Loop
+colors = ["blue","green","red","purple"]
+for color in colors:
+    print("the current color is: " + color)
+
+# for loop using dict
+ages = {'Richard': 50, 'Nina': 40, 'Mathew': 20}
+for key, value in ages.items():
+    print("The key is: " + key)
+    print("The value is: " + str(value))
+
