06c583f4b930f819a28357e1243cca2f9685cd67
Richard Ramirez <hackysmack69@gmail.com>
2022-09-17 18:18:28 -0500

    updated comprehensions and redo of lab

--- ch14/comprehensions.py
+++ ch14/comprehensions.py
@@ -5,9 +5,16 @@ for color in colors:
 print("The uppercase colors are: ")
 print(uppercase_colors)
 
+# the comprehension
 colors2 = ["red","blue","orange","green","yellow"]
 uppercase_colors2 = [color.upper() for color in colors]
 
 print("The uppercase colors 2 are: ")
 print(uppercase_colors2)
+
+# comprehension with if
+
+colors3 = ["red","blue","orange","green","yellow"]
+warm_colors = [color for color in colors if color in["red","orange","yellow"]]
+
 
